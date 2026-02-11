c847971203a08e53975a46de2b879efac8707ed8
EmoneyCode <trame718@gmail.com>
2026-02-11 00:10:53 -0600

    this is the 2nd question which will decrypt to get the plain text

--- decrypt.java
+++ decrypt.java
@@ -16,7 +16,53 @@ public class decrypt {
         }
         return CipherText(cipher1) + CipherText(cipher2);
     }
+
+    public static String decryptString(String cipher){
+        int n = cipher.length();
+
+        // Base case
+        if (n < 3) {
+            return cipher;
+        }
+
+        // Determine subtree sizes
+        int leftSize = (n + 1) / 2;  // ceil(n/2)
+        int rightSize = n / 2;       // floor(n/2)
+
+        // Split ciphertext into left and right parts
+        String leftCipher = cipher.substring(0, leftSize);
+        String rightCipher = cipher.substring(leftSize);
+
+        // Recursively decrypt each half
+        String leftPlain = decryptString(leftCipher);
+        String rightPlain = decryptString(rightCipher);
+
+        // Interleave to reconstruct original order
+        return interleave(leftPlain, rightPlain);
+    }
+
+    private static String interleave(String left, String right) {
+        StringBuilder result = new StringBuilder();
+
+        int i = 0, j = 0;
+
+        while (i < left.length() || j < right.length()) {
+
+            if (i < left.length()) {
+                result.append(left.charAt(i));
+                i++;
+            }
+
+            if (j < right.length()) {
+                result.append(right.charAt(j));
+                j++;
+            }
+        }
+
+        return result.toString();
+    }
     public static void main(String args[]){
         System.out.println(CipherText("THISCOURSEISANINTROTOCOMPUTERSECURITYWITHANEMPHASISONSOFTWAREDESIGNPRINCIPLESANDTECHNICALCONTROLS"));
+        System.out.println(decryptString("TRMENBDEEVPUTYSHOECELIOAARISP"));
     }
 }
