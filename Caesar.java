d9aaa16ae2da7870d0b49d19ddac5e15e58f44d2
calebwisneski-ops <caleb.wisneski@gmail.com>
2026-02-16 23:41:34 -0600

    changed spacing

--- Caesar.java
+++ Caesar.java
@@ -8,79 +8,79 @@
 class Caesar
 {
 
-    /* This method takes in a Caesar-encrypted message and the key used 
-       to generate this ciphertext. It returns the original plaintext.
-       The third argument determines whether the plaintext should be
-       printed before returning from the method. If its value is true, 
-       your code should print the value of the key, followed by a single
-       white space, followed by the plaintext, followed by a single newline
-       character. If its value is false, the method should return with no
-       printed output.
-     */
-    static String decrypt(String ciphertext, int key, 
-                          boolean printPlaintext)
+  /* This method takes in a Caesar-encrypted message and the key used 
+      to generate this ciphertext. It returns the original plaintext.
+      The third argument determines whether the plaintext should be
+      printed before returning from the method. If its value is true, 
+      your code should print the value of the key, followed by a single
+      white space, followed by the plaintext, followed by a single newline
+      character. If its value is false, the method should return with no
+      printed output.
+    */
+  static String decrypt(String ciphertext, int key, 
+                        boolean printPlaintext)
+  {
+    String plaintext = "";
+    for(int i = 0; i < ciphertext.length(); i++){
+      int ascii = (int)ciphertext.charAt(i) - key;
+      if(ascii < 65){
+        ascii += 26;
+      }
+      plaintext += (char)ascii;
+    }
+    if(printPlaintext){
+      System.out.println(key + " " + plaintext);
+    }
+  return plaintext;
+  }// decrypt method
+
+  /* This method will be used for testing purposes. You must complete it
+      without deleting or modifying the provided code. This method must:
+      1. load the dictionary and the ciphertext given as command-line arguments
+      2. decrypt the ciphertext with all reasonable keys and print the
+        plaintext that contains the largest number of dictionary words.
+      Note that, besides the messages pertaining to the I/O operations on 
+      the dictionary and ciphertext files, this method must print only a
+      single line of text, namely the recovered plaintext, followed by a
+      single newline character. In other words, the decrypt method must be
+      called with 'false' as its third argument. During grading, I will
+      change this value to 'true' to check the intermediate behavior of your
+      code.
+    */
+  public static void main(String[] args)
+  {   
+    if (args.length != 2)
     {
-        String plaintext = "";
-        for(int i = 0; i < ciphertext.length(); i++){
-            int ascii = (int)ciphertext.charAt(i) - key;
-            if(ascii < 65){
-                ascii += 26;
-            }
-            plaintext += (char)ascii;
-        }
-        if(printPlaintext){
-            System.out.println(key + " " + plaintext);
-        }
-	    return plaintext;
-    }// decrypt method
+        System.out.println("This program should be invoked with the " +
+                            "following arguments:");
+        System.out.println("  java Cesar <dictionary file name> " +
+                            "<ciphertext file name>");
+        System.exit(1);
+    }
 
-    /* This method will be used for testing purposes. You must complete it
-       without deleting or modifying the provided code. This method must:
-       1. load the dictionary and the ciphertext given as command-line arguments
-       2. decrypt the ciphertext with all reasonable keys and print the
-          plaintext that contains the largest number of dictionary words.
-       Note that, besides the messages pertaining to the I/O operations on 
-       the dictionary and ciphertext files, this method must print only a
-       single line of text, namely the recovered plaintext, followed by a
-       single newline character. In other words, the decrypt method must be
-       called with 'false' as its third argument. During grading, I will
-       change this value to 'true' to check the intermediate behavior of your
-       code.
-     */
-    public static void main(String[] args)
-    {   
-        if (args.length != 2)
-        {
-            System.out.println("This program should be invoked with the " +
-                               "following arguments:");
-            System.out.println("  java Cesar <dictionary file name> " +
-                               "<ciphertext file name>");
-            System.exit(1);
-        }
+    String ciphertext = Utils.loadFromFile("ciphertext",args[1]);
 
-        String ciphertext = Utils.loadFromFile("ciphertext",args[1]);
+    //Load Dict
+    Dict dict = new Dict(args[0]);
 
-        //Load Dict
-        Dict dict = new Dict(args[0]);
+    int maxCount = -1;
+    String bestPlaintext = "";
 
-        int maxCount = -1;
-        String bestPlaintext = "";
+    //loop through all possible keys
+    for(int i = 0; i < 26; i++){
+      String plaintext = decrypt(ciphertext, i, false);
 
-        //loop through all possible keys
-        for(int i = 0; i < 26; i++){
-            String plaintext = decrypt(ciphertext, i, false);
+      int count = dict.countWords(plaintext);
 
-            int count = dict.countWords(plaintext);
+      if (count > maxCount) {
+        maxCount = count;
+        bestPlaintext = plaintext;
+      }
 
-            if (count > maxCount) {
-                maxCount = count;
-                bestPlaintext = plaintext;
-            }
+    }
 
-        }
+    System.out.println(bestPlaintext);
 
-        System.out.println(bestPlaintext);
-
-    }// main method
-    
+  }// main method
+  
 }// Caesar class
--- Dict.java
+++ Dict.java
@@ -10,98 +10,98 @@ import java.io.*;
 
 class Dict
 {
-    int numWords;    // the number of words in the dictionary
-    String[] words;  // the words included in the dictionary
-    int maxLength;   // the number of characters in the longest word
+  int numWords;    // the number of words in the dictionary
+  String[] words;  // the words included in the dictionary
+  int maxLength;   // the number of characters in the longest word
 
-    /* This constructor loads the words from the file whose name is passed
-       as an argument. In this text file, there is exactly one word per line.
-       This constructor initializes the three instance variables above.
-       Note that the words must be stored in alphabetical order in the array 
-       and that the length of the array must be equal to the number of words 
-       in the input file.
-    */
-    Dict(String fileName)
-    {
-        System.out.print("Loading dictionary... ");
-        ArrayList<String> wordArrayList = new ArrayList<>();
-        maxLength = 0;
-        File file = new File(fileName);
-        try{
-            for(int i = 65; i < 91; i++){
+  /* This constructor loads the words from the file whose name is passed
+      as an argument. In this text file, there is exactly one word per line.
+      This constructor initializes the three instance variables above.
+      Note that the words must be stored in alphabetical order in the array 
+      and that the length of the array must be equal to the number of words 
+      in the input file.
+  */
+  Dict(String fileName)
+  {
+    System.out.print("Loading dictionary... ");
+    ArrayList<String> wordArrayList = new ArrayList<>();
+    maxLength = 0;
+    File file = new File(fileName);
+    try{
+      for(int i = 65; i < 91; i++){
 
-                Scanner sc = new Scanner(file);
-                while(sc.hasNextLine()){
-                    String word = sc.nextLine().toUpperCase();
-                    if((char)i == word.charAt(0)){
-                        wordArrayList.add(word);
-                        numWords++;
-                        maxLength = Math.max(maxLength, word.length());
-                    }
-                }
+          Scanner sc = new Scanner(file);
+          while(sc.hasNextLine()){
+            String word = sc.nextLine().toUpperCase();
+            if((char)i == word.charAt(0)){
+              wordArrayList.add(word);
+              numWords++;
+              maxLength = Math.max(maxLength, word.length());
             }
-            words = wordArrayList.toArray(new String[0]);
-            
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
+          }
+      }
+      words = wordArrayList.toArray(new String[0]);
         
-
-        System.out.println("done");
-    }// constructor
-
-    /* This method returns true if and only if the given word is contained
-       in the dictionary. 
-    */
-    boolean contains(String word)
-    {
-        for(int i = 0; i < words.length; i++){
-            if(word.equals(words[i])){
-                return true;
-            }
-        }
-
-        return false;
-    }// contains method
+    }catch(Exception e){
+        System.out.println(e.getMessage());
+    }
     
-    /* This method returns the number of occurrences of dictionary words  
-       in the given text. To be clear, if a dictionary word appears n times
-       in the text, then the final count returned by this method  must be 
-       incremented by n. Note that two or more words may overlap in the text.
-       For example, the text "the bananas" contains the words: 
-          "a" "an" "as" "ban" "banana" "bananas" among others            
-     */
-    int countWords(String text)
-    {
-        int count = 0;
-        for(int i = 0; i < words.length; i++){
-            int startPoint = 0;
-            while(startPoint + words[i].length() <= text.length()){
-                int index = text.substring(startPoint).indexOf(words[i]);
-                //if the word is found move the starting point to after where it was found in text and add to count
-                if(index != -1){
-                    count++;
-                    startPoint = startPoint + index + 1;
-                } else {
-                    break;
-                }
-            }
-        }
 
-        return count;
-    }// countWords method
+    System.out.println("done");
+  }// constructor
 
-    /* This method will only be used for testing purposes 
-       You may NOT modify it.
-     */
-    public static void main(String[] args)
-    {   
-        Dict d = new Dict(args[0]);
-        System.out.println(d.numWords + " " + d.maxLength);
-        for(int index=0; index<d.words.length; index++) {
-            System.out.print(d.words[index] + " ");
+  /* This method returns true if and only if the given word is contained
+      in the dictionary. 
+  */
+  boolean contains(String word)
+  {
+    for(int i = 0; i < words.length; i++){
+      if(word.equals(words[i])){
+        return true;
+      }
+    }
+
+    return false;
+  }// contains method
+  
+  /* This method returns the number of occurrences of dictionary words  
+      in the given text. To be clear, if a dictionary word appears n times
+      in the text, then the final count returned by this method  must be 
+      incremented by n. Note that two or more words may overlap in the text.
+      For example, the text "the bananas" contains the words: 
+        "a" "an" "as" "ban" "banana" "bananas" among others            
+    */
+  int countWords(String text)
+  {
+    int count = 0;
+    for(int i = 0; i < words.length; i++){
+      int startPoint = 0;
+      while(startPoint + words[i].length() <= text.length()){
+        int index = text.substring(startPoint).indexOf(words[i]);
+        //if the word is found move the starting point to after where it was found in text and add to count
+        if(index != -1){
+          count++;
+          startPoint = startPoint + index + 1;
+        } else {
+          break;
         }
-        System.out.println(d.words[d.numWords-1]);
-    }// main method
+      }
+    }
+
+    return count;
+  }// countWords method
+
+  /* This method will only be used for testing purposes 
+      You may NOT modify it.
+    */
+  public static void main(String[] args)
+  {   
+    Dict d = new Dict(args[0]);
+    System.out.println(d.numWords + " " + d.maxLength);
+    for(int index=0; index<d.words.length; index++) {
+      System.out.print(d.words[index] + " ");
+    }
+    System.out.println(d.words[d.numWords-1]);
+  }// main method
 
 }// Dict class
